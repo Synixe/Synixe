203ee0641d3b378a616dd5ab18d51291fee2fc87
Brett <brett@bmandesigns.com>
2019-09-17 22:30:22 -0600

    loadouts example config

---
+++ addons/loadouts/XEH_postInit.sqf
@@ -0,0 +1,7 @@
+#include "script_component.hpp"
+
+// private _loadouts = configProperties [configFile >> "Babel" >> "Languages", "true", true];
+// {
+//   [configName _x, getText _x] call acre_api_fnc_babelAddLanguageType;
+//   _allLoado pushBack (configName _x);
+// } forEach _loadouts;
---
+++ addons/loadouts/loadouts/NATO.hpp
@@ -0,0 +1,17 @@
+class USMC {
+    class Woodland {
+        // Becomes: USMC Woodland - Squad Leader
+        Squad_Leader = QUOTE();
+    };
+};
+
+class Canada {
+    class TW {
+        // Becomes: Canada TW - Squad Leader
+        Squad_Leader = QUOTE();
+    };
+    class AR {
+        // Becomes: Canada AR - Squad Leader
+        Squad_Leader = QUOTE();
+    };
+};
