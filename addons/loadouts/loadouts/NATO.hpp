e7b767e02abc85f05ea022a680019e2be7c21ff8
Brett <brett@bmandesigns.com>
2019-09-20 05:13:09 -0600

    german loadouts

--- addons/loadouts/loadouts/NATO.hpp
+++ addons/loadouts/loadouts/NATO.hpp
@@ -1,23 +1,3 @@
-class USMC {
-    class Woodland {
-        // Becomes: USMC Woodland - Squad Leader
-        Squad_Leader = QUOTE();
-    };
-};
-
-class Canada {
-    class TW {
-        // Becomes: Canada TW - Squad Leader
-        Squad_Leader = QUOTE();
-        // Becomes: Canada TW - Team Leader
-        Team_Leader = QUOTE();
-    };
-    class AR {
-        // Becomes: Canada AR - Squad Leader
-        Squad_Leader = QUOTE();
-    };
-};
-
 class German {
     class Flecktarn {
         Squad_Leader = QUOTE([["CUP_arifle_AG36_wdl","","","",["CUP_30Rnd_556x45_G36_wdl",30],["1Rnd_HE_Grenade_shell",1],""],[],["hgun_P07_F","","","",["16Rnd_9x21_Mag",17],[],""],["CFP_U_Crye_Flecktarn",[["ACRE_PRC148",1],["ACRE_PRC343",1],["ACE_elasticBandage",5],["ACE_tourniquet",4],["ACE_CableTie",2],["16Rnd_9x21_Mag",2,17],["HandGrenade",1,1]]],["CFP_PlateCarrier1_Flecktarn",[["CUP_30Rnd_556x45_G36_wdl",7,30],["1Rnd_HE_Grenade_shell",5,1],["1Rnd_Smoke_Grenade_shell",2,1],["UGL_FlareGreen_F",1,1],["UGL_FlareRed_F",1,1],["UGL_FlareYellow_F",1,1]]],["CFP_AssaultPack_Flecktarn",[["ACE_EntrenchingTool",1],["ACE_wirecutter",1],["ACRE_VHF30108SPIKE",1]]],"CFP_OpsC_Med_KSK2","",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","","ItemCompass","ItemWatch",""]]);
@@ -25,7 +5,7 @@ class German {
         Grenadier = QUOTE([["CUP_arifle_AG36_wdl","","","",["CUP_30Rnd_556x45_G36_wdl",30],["1Rnd_HE_Grenade_shell",1],""],[],["hgun_P07_F","","","",["16Rnd_9x21_Mag",17],[],""],["CFP_U_Crye_Flecktarn",[["ACRE_PRC343",1],["ACE_elasticBandage",5],["ACE_tourniquet",4],["ACE_CableTie",2],["16Rnd_9x21_Mag",2,17],["HandGrenade",1,1]]],["CFP_PlateCarrier1_Flecktarn",[["CUP_30Rnd_556x45_G36_wdl",7,30],["1Rnd_HE_Grenade_shell",5,1],["1Rnd_Smoke_Grenade_shell",2,1],["UGL_FlareGreen_F",1,1],["UGL_FlareRed_F",1,1],["UGL_FlareYellow_F",1,1]]],[],"CFP_OpsC_Med_KSK2","",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","","ItemCompass","ItemWatch",""]]);
         Rifleman = QUOTE([["CUP_arifle_G36C_wdl","","","",["CUP_30Rnd_556x45_G36_wdl",30],[],""],[],["hgun_P07_F","","","",["16Rnd_9x21_Mag",17],[],""],["CFP_U_Crye_Flecktarn",[["ACRE_PRC343",1],["ACE_elasticBandage",5],["ACE_tourniquet",4],["ACE_CableTie",2],["16Rnd_9x21_Mag",2,17],["HandGrenade",1,1]]],["CFP_PlateCarrier1_Flecktarn",[["CUP_30Rnd_556x45_G36_wdl",10,30]]],[],"CFP_OpsC_Med_KSK2","",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","","ItemCompass","ItemWatch",""]]);
         Rifleman_LAT = QUOTE([["CUP_arifle_G36C_wdl","","","",["CUP_30Rnd_556x45_G36_wdl",30],[],""],["CUP_launch_M136","","","",["ACE_PreloadedMissileDummy_CUP",1],[],""],["hgun_P07_F","","","",["16Rnd_9x21_Mag",17],[],""],["CFP_U_Crye_Flecktarn",[["ACE_elasticBandage",5],["ACE_tourniquet",4],["ACE_CableTie",2],["ACRE_PRC343",1],["16Rnd_9x21_Mag",2,17],["HandGrenade",1,1]]],["CFP_PlateCarrier1_Flecktarn",[["CUP_30Rnd_556x45_G36_wdl",10,30]]],[],"CFP_OpsC_Med_KSK2","",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","","ItemCompass","ItemWatch",""]]);
-        Rifleman HAT = QUOTE([["CUP_arifle_G36C_wdl","","","",["CUP_30Rnd_556x45_G36_wdl",30],[],""],["launch_I_Titan_short_F","","","",["Titan_AT",1],[],""],["hgun_P07_F","","","",["16Rnd_9x21_Mag",17],[],""],["CFP_U_Crye_Flecktarn",[["ACE_elasticBandage",5],["ACE_tourniquet",4],["ACE_CableTie",2],["ACRE_PRC343",1],["16Rnd_9x21_Mag",2,17],["HandGrenade",1,1]]],["CFP_PlateCarrier1_Flecktarn",[["CUP_30Rnd_556x45_G36_wdl",10,30]]],["CFP_Kitbag_Flecktarn",[["Titan_AT",2,1]]],"CFP_OpsC_Med_KSK2","",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","","ItemCompass","ItemWatch",""]]);
+        Rifleman_HAT = QUOTE([["CUP_arifle_G36C_wdl","","","",["CUP_30Rnd_556x45_G36_wdl",30],[],""],["launch_I_Titan_short_F","","","",["Titan_AT",1],[],""],["hgun_P07_F","","","",["16Rnd_9x21_Mag",17],[],""],["CFP_U_Crye_Flecktarn",[["ACE_elasticBandage",5],["ACE_tourniquet",4],["ACE_CableTie",2],["ACRE_PRC343",1],["16Rnd_9x21_Mag",2,17],["HandGrenade",1,1]]],["CFP_PlateCarrier1_Flecktarn",[["CUP_30Rnd_556x45_G36_wdl",10,30]]],["CFP_Kitbag_Flecktarn",[["Titan_AT",2,1]]],"CFP_OpsC_Med_KSK2","",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","","ItemCompass","ItemWatch",""]]);
     };
     class Tropentarn {
         Squad_Leader = QUOTE([["CUP_arifle_AG36_camo","","","",["CUP_30Rnd_556x45_G36_camo",30],["1Rnd_HE_Grenade_shell",1],""],[],["hgun_P07_F","","","",["16Rnd_9x21_Mag",17],[],""],["CFP_U_Crye_Tropentarn",[["ACE_elasticBandage",5],["ACE_tourniquet",4],["ACE_CableTie",2],["ACRE_PRC148",1],["ACRE_PRC343",1],["16Rnd_9x21_Mag",2,17],["HandGrenade",1,1]]],["CFP_PlateCarrier1_Tropentarn",[["CUP_30Rnd_556x45_G36_camo",7,30],["1Rnd_HE_Grenade_shell",4,1],["1Rnd_Smoke_Grenade_shell",2,1],["UGL_FlareGreen_F",1,1],["UGL_FlareRed_F",1,1],["UGL_FlareYellow_F",1,1]]],["CFP_AssaultPack_Tropentarn",[["ACE_EntrenchingTool",1],["ACE_wirecutter",1],["ACRE_VHF30108SPIKE",1]]],"CFP_OpsC_Covered_KSK","",["Binocular","","","",[],[],""],["ItemMap","ItemGPS","","ItemCompass","ItemWatch",""]]);
