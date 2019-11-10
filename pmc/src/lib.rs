caffc28e9c5d95e8e9d9727382c46d532d504dd5
Brett <brett@bmandesigns.com>
2019-11-09 23:42:36 -0600

    fmt

--- pmc/src/lib.rs
+++ pmc/src/lib.rs
@@ -10,12 +10,16 @@ extern crate lazy_static;
 extern crate mysql;
 
 lazy_static! {
-    static ref POOL: Mutex<mysql::Pool> = Mutex::new(mysql::Pool::new(std::env::var("PMC_HOST").unwrap().to_string()).unwrap());
+    static ref POOL: Mutex<mysql::Pool> =
+        Mutex::new(mysql::Pool::new(std::env::var("PMC_HOST").unwrap().to_string()).unwrap());
 }
 
 #[rv]
 fn save_loadout(player: String, loadout: String) {
-    POOL.lock().unwrap().prepare(r##"
+    POOL.lock()
+        .unwrap()
+        .prepare(
+            r##"
     INSERT INTO pmc.loadouts
         (player, loadout)
     VALUES
@@ -23,20 +27,28 @@ fn save_loadout(player: String, loadout: String) {
     ON DUPLICATE KEY UPDATE
         player = :player,
         loadout = :loadout;
-  "##).unwrap().execute(params!{
-    "player" => player,
-    "loadout" => loadout,
-  }).unwrap();
+  "##,
+        )
+        .unwrap()
+        .execute(params! {
+          "player" => player,
+          "loadout" => loadout,
+        })
+        .unwrap();
 }
 
 #[rv]
 fn get_loadout(player: String) -> String {
-    let res = POOL.lock().unwrap().prep_exec(
-        "SELECT loadout FROM pmc.loadouts WHERE (loadouts.player = :player) LIMIT 1",
-        params!{
-            "player" => player,
-        }
-    ).unwrap();
+    let res = POOL
+        .lock()
+        .unwrap()
+        .prep_exec(
+            "SELECT loadout FROM pmc.loadouts WHERE (loadouts.player = :player) LIMIT 1",
+            params! {
+                "player" => player,
+            },
+        )
+        .unwrap();
     if res.more_results_exists() {
         for row in res {
             let loadout = mysql::from_row(row.unwrap());
@@ -48,7 +60,10 @@ fn get_loadout(player: String) -> String {
 
 #[rv]
 fn save_variable(player: String, name: String, value: String) {
-    POOL.lock().unwrap().prepare(r##"
+    POOL.lock()
+        .unwrap()
+        .prepare(
+            r##"
         INSERT INTO pmc.variables
             (player, name, value)
         VALUES
@@ -57,21 +72,29 @@ fn save_variable(player: String, name: String, value: String) {
             player = :player,
             name = :name,
             value = :value
-    "##).unwrap().execute(params!{
-        "player" => player,
-        "name" => name,
-        "value" => value,
-    }).unwrap();
+    "##,
+        )
+        .unwrap()
+        .execute(params! {
+            "player" => player,
+            "name" => name,
+            "value" => value,
+        })
+        .unwrap();
 }
 
 #[rv]
 fn get_variables(player: String) -> String {
-    let res = POOL.lock().unwrap().prep_exec(
-        "SELECT name, value FROM pmc.variables WHERE (variables.player = :player)",
-        params!{
-            "player" => player,
-        }
-    ).unwrap();
+    let res = POOL
+        .lock()
+        .unwrap()
+        .prep_exec(
+            "SELECT name, value FROM pmc.variables WHERE (variables.player = :player)",
+            params! {
+                "player" => player,
+            },
+        )
+        .unwrap();
     let mut text = String::from("[");
     let mut first = true;
     for row in res {
@@ -89,21 +112,39 @@ fn get_variables(player: String) -> String {
 #[rv_handler]
 unsafe fn init() {
     thread::spawn(|| loop {
-        POOL.lock().unwrap().prep_exec(r##"
+        POOL.lock()
+            .unwrap()
+            .prep_exec(
+                r##"
             CREATE DATABASE IF NOT EXISTS pmc;
-            "##, ()).unwrap();
-            POOL.lock().unwrap().prep_exec(r##"
-            CREATE TABLE IF NOT EXISTS pmc.loadouts (
-                player varchar(64) not null,
-                loadout text,
-                primary key (player)
-            )"##, ()).unwrap();
-            POOL.lock().unwrap().prep_exec(r##"
-            CREATE TABLE IF NOT EXISTS pmc.variables (
-                player varchar(64) not null,
-                name varchar(128) not null,
-                value text,
-                primary key (player, name)
-            )"##, ()).unwrap();
+            "##,
+                (),
+            )
+            .unwrap();
+        POOL.lock()
+            .unwrap()
+            .prep_exec(
+                r##"
+        CREATE TABLE IF NOT EXISTS pmc.loadouts (
+            player varchar(64) not null,
+            loadout text,
+            primary key (player)
+        )"##,
+                (),
+            )
+            .unwrap();
+        POOL.lock()
+            .unwrap()
+            .prep_exec(
+                r##"
+        CREATE TABLE IF NOT EXISTS pmc.variables (
+            player varchar(64) not null,
+            name varchar(128) not null,
+            value text,
+            primary key (player, name)
+        )"##,
+                (),
+            )
+            .unwrap();
     });
 }
